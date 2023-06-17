78a58206fbc958d98326e321ce313e033045bd5c
Juan Vanegas <juan.vanegas2@mail.mcgill.ca>
2023-06-17 19:09:35 -0400

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import "./app.scss";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("todo-items")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todo-items", JSON.stringify(items));
+  }, [items]);
+
   return (
     <div id="app">
       <h1>My Todo 🗒️</h1>
@@ -12,14 +22,54 @@ function App() {
             type="text"
             id="title"
             placeholder="Title"
-            class="form-control"
-            autocomplete="off"
+            className="form-control"
+            autoComplete="off"
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+            value={input}
           />
-          <button>Add</button>
+          <button
+            type="button"
+            onClick={(event) => {
+              if (!input) {
+                alert("value cannot be empty");
+                return;
+              }
+              const newItems = [...items];
+              newItems.push(input);
+              setItems(newItems);
+              setInput("");
+            }}
+          >
+            Add
+          </button>
         </form>
 
         <div className="items">
           <h2>My Items</h2>
+          {
+            <ol>
+              {items.map((item, index) => {
+                return (
+                  <li key={`item-${index}`}>
+                    <h3>{item}</h3>
+
+                    <button
+                      className="delete"
+                      onClick={() => {
+                        const newItems = items.filter((item, indexItem) => {
+                          return index !== indexItem;
+                        });
+
+                        setItems(newItems);
+                      }}
+                    ></button>
+                  </li>
+                );
+              })}
+            </ol>
+          }
         </div>
       </div>
     </div>
